44ff285809d465010b5fb76e5fe282af8c08ac4f
THEM0R <the.mor.ua@gmail.com>
2023-03-18 18:40:47 +0200

    init composer.json 0.0.2

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 
-echo 'PHP-DI - сервіс контейнер';
+require __DIR__ . '/vendor/autoload.php';
 
+echo '<a href="https://www.youtube.com/watch?v=4DkvZ3RTFm8">PHP-DI - сервіс контейнер</a>';
+
+class A
+{
+  private $b;
+
+  public function __construct(B $b)
+  {
+    $this->b = $b;
+  }
+
+}
+
+class B
+{
+
+}
